f94095d785d131ac38536dd1ffe58a318996baa5
Abhijeet Kasurde <akasurde@redhat.com>
2016-01-04 14:16:43 +0530

    Cli app
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ cliapp/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "os"
+    "github.com/codegangsta/cli"
+    )
+
+func main() {
+    app := cli.NewApp()
+    app.Name = "Sample Cli app"
+    app.Usage = "A sample cli App"
+    app.Action = func(c *cli.Context) {
+        println("Hello World from Cli app")
+    }
+    app.Run(os.Args)
+}
